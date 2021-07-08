bde56e707ead6b163412534ff4b619301b67ec6f
DIR\vincenzo.scalcione <vincenzo.scalcione@accenture.com>
2021-07-08 15:23:02 +0200

    Initial commit

---
+++ employee-manager-backend/src/main/java/it/vscalcione/springboot/employeemanager/EmployeeManagerBackendApplication.java
@@ -0,0 +1,13 @@
+package it.vscalcione.springboot.crudappbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManagerBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudappBackendApplication.class, args);
+	}
+
+}
